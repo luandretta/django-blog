67376e68d7f5b5d76cd99bdbd13fddd9d5a1ebfa
Lucimeri Andretta <luandretta@hotmail.com>
2023-03-13 14:31:51 +0000

    Feat: create PostDetail

--- blog/admin.py
+++ blog/admin.py
@@ -16,7 +16,6 @@ class PostAdmin(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
