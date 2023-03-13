9b87794847f2149b5336f2ecfba9d461d8a0390f
Lucimeri Andretta <luandretta@hotmail.com>
2023-03-13 12:49:22 +0000

    Feat: create the first view
    Create base, index and post_detail.html and style.css

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
+
 # admin.site.register(Post)
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
